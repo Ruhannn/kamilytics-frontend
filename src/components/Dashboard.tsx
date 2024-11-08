f46a4f71c38487a956787a54f052ceb40759a5c8
Ruhannn <ruhanrouf17@gmail.com>
2024-11-08 09:10:39 +0600

    added icons

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,36 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "react-query";
 import { getData } from "../service";
 import { GetData } from "../@types";
 import { calculatePercentage } from "../utils/calculatePercentage";
-
-
-
-interface DataSectionProps {
-  title: string;
-  items: { name: string; count: number }[];
-}
-
-const DataSection: React.FC<DataSectionProps> = ({ title, items }) => {
-  const total = items.reduce((sum, item) => sum + item.count, 0);
-
-  return (
-    <div className="p-4 rounded-lg bg-secondary">
-      <h2 className="mb-2 text-lg font-semibold">{title}</h2>
-      <ul>
-        {items
-          .sort((a, b) => b.count - a.count)
-          .map((item, index) => (
-            <li key={index} className="flex justify-between py-1">
-              <span>{item.name}</span>
-              <span>
-                {item.count} ({calculatePercentage(item.count, total)}%)
-              </span>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+import { Desktop, Mobile, Tablet } from "../icons";
+import { capitalizeFirstLetter } from "../utils/capitalizeFirstLetter";
+import { CountryFlag } from "react-countryname-flag";
 
 export default function Dashboard() {
   const { data, isLoading } = useQuery<GetData>({
@@ -70,7 +45,7 @@ export default function Dashboard() {
             count,
           }))}
         />
-        <DataSection
+        <CountrySection
           title="Countries"
           items={data.countries.map(({ country, count }) => ({
             name: country,
@@ -81,3 +56,178 @@ export default function Dashboard() {
     </div>
   );
 }
+
+interface DataSectionProps {
+  title: string;
+  items: { name: string; count: number }[];
+}
+
+const DataSection: React.FC<DataSectionProps> = ({ title, items }) => {
+  const total = items.reduce((sum, item) => sum + item.count, 0);
+  return (
+    <div className="p-4 rounded-lg shadow-md bg-secondary">
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <span className="text-lg font-semibold text-text">Visitors</span>
+      </div>
+      <ul>
+        {items
+          .sort((a, b) => b.count - a.count)
+          .map((item, index) => {
+            let name: null | string = item.name.toLowerCase();
+            let img: any | string;
+
+            switch (name) {
+              // os
+              case "android os":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/4425b8ae65b41da776b1d24d364f6f02_Xgx04UeAwU.png";
+                name = "Android";
+                break;
+              case "linux":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/6984223cd33751654099ea66970bcec8_kfekaJmEYO.png";
+                name = "Linux";
+                break;
+              case "mac-os":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/a1ba5ace1d63d4cf99d45e2d0ba8c7c3_low_res_Apple.png";
+                name = "macOS";
+                break;
+              case "windows 10":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/b897cb95c17c66b561f3f1054dc648fa_low_res_Windows_11_Parallels.png";
+                name = "Windows 10/11";
+                break;
+              // browser
+              case "samsung":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/8d38cf21eb0825a1c9fcb4edd39d7dbe_41CQNsdTyP.png";
+                name = "Samsung";
+                break;
+              case "chromium-webview":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/b83b21d52d1f44d00520de6f9e396f3d_sjBKtSgMwR.png";
+                name = "Chrome (webview)";
+                break;
+              case "yandexbrowser":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/a7cae77c48ae88d5017964a8a38d8b50_FLrkFgdgER.png";
+                name = "Yandex";
+                break;
+              case "opera":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/18cce2902e8a65cb51a66d93f6a9c7fa_i8GlntZO53.png";
+                name = "Opera";
+                break;
+              case "edge-chromium":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/9ebaaddcde54291b5e3608574598d347_oha7s7NpIj.png";
+                name = "Edge (Chromium)";
+                break;
+              case "safari":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/8204ffaf2c6f9f46a1a803a96c91e7d5_low_res_Safari.png";
+                name = "Safari";
+                break;
+              case "ios":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/4eeffffb168a66d18155e133397d5111_DiPPbQPhrD.png";
+                name = "iOS";
+                break;
+              case "ios-webview":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/4eeffffb168a66d18155e133397d5111_DiPPbQPhrD.png";
+                name = "iOS (webview)";
+                break;
+              case "chrome":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/a96581679ddd7c7ae6233c04896eb339_low_res_Google_Chrome.png";
+                name = "Chrome";
+                break;
+              case "firefox":
+                img =
+                  "https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/578112c32e9d1f0e18de77c04e1e00ca_low_res_Firefox_Icon_Gradient_Version.png";
+                name = "Firefox";
+                break;
+              // device type
+              case "mobile":
+                img = Mobile;
+                name = "Mobile";
+                break;
+              case "desktop":
+                img = Desktop;
+                name = "Desktop";
+                break;
+              case "tablet":
+                img = Tablet;
+                name = "Tablet";
+                break;
+              default:
+                img = null;
+                name = null;
+                break;
+            }
+            return (
+              <li
+                key={index}
+                className="flex items-center justify-between py-2 border-b border-gray-200 last:border-0"
+              >
+                <div className="flex items-center space-x-2 [&_img]:size-5">
+                  {img && typeof img === "string" ? (
+                    <img src={img} alt={item.name} />
+                  ) : (
+                    <div>{img}</div>
+                  )}
+
+                  <span className="text-sm font-medium">
+                    {name ?? capitalizeFirstLetter(item.name)}
+                  </span>
+                </div>
+                <span className="text-sm text-text">
+                  {item.count} ({calculatePercentage(item.count, total)}%)
+                </span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
+const CountrySection: React.FC<DataSectionProps> = ({ title, items }) => {
+  const total = items.reduce((sum, item) => sum + item.count, 0);
+
+  return (
+    <div className="p-4 rounded-lg bg-secondary">
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <span className="text-lg font-semibold text-text">Visitors</span>
+      </div>
+      <ul>
+        {items
+          .sort((a, b) => b.count - a.count)
+          .map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="flex items-center justify-between py-2 border-b border-gray-200 last:border-0"
+              >
+                <div className="flex items-center space-x-2">
+                  <CountryFlag
+                    countryName={item.name}
+                    style={{ fontSize: "20px" }}
+                  />
+                  <span className="text-sm font-medium">
+                    {capitalizeFirstLetter(item.name)}
+                  </span>
+                </div>
+                <span className="text-sm text-text">
+                  {item.count} ({calculatePercentage(item.count, total)}%)
+                </span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
---
+++ src/icons/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+export const Mobile = (
+  <>
+    <svg
+      //   style="color:#AB92F0;"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M14.5 0H5.5C4.39543 0 3.5 0.89543 3.5 2V18C3.5 19.1046 4.39543 20 5.5 20H14.5C15.6046 20 16.5 19.1046 16.5 18V2C16.5 0.89543 15.6046 0 14.5 0ZM5.5 18V2H14.5V18H5.5Z"
+          fill="currentColor"
+        />
+        <path
+          d="M10 17.25C9.30964 17.25 8.75 16.6904 8.75 16C8.75 15.3096 9.30964 14.75 10 14.75C10.6904 14.75 11.25 15.3096 11.25 16C11.25 16.6904 10.6904 17.25 10 17.25Z"
+          fill="currentColor"
+        />
+      </g>
+    </svg>
+  </>
+);
+export const Tablet = (
+  <>
+    <svg
+      //   style="color:#AB92F0;"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M1.5 2C1.5 0.895432 2.39543 0 3.5 0H16.5C17.6046 0 18.5 0.895431 18.5 2V18C18.5 19.1046 17.6046 20 16.5 20H3.5C2.39543 20 1.5 19.1046 1.5 18V2ZM16.5 2L3.5 2V18H16.5V2Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M7.75 16C7.75 15.4477 8.19772 15 8.75 15H11.25C11.8023 15 12.25 15.4477 12.25 16C12.25 16.5523 11.8023 17 11.25 17H8.75C8.19772 17 7.75 16.5523 7.75 16Z"
+        fill="currentColor"
+      />
+    </svg>
+  </>
+);
+export const Desktop = (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M17.5 1H2.5C1.11929 1 0 2.11929 0 3.5V13.5C0 14.8807 1.11929 16 2.5 16H17.5C18.8807 16 20 14.8807 20 13.5V3.5C20 2.11929 18.8807 1 17.5 1ZM2 3.5C2 3.22386 2.22386 3 2.5 3H17.5C17.7761 3 18 3.22386 18 3.5V13.5C18 13.7761 17.7761 14 17.5 14H2.5C2.22386 14 2 13.7761 2 13.5V3.5Z"
+      fill="currentColor"
+    />
+    <path
+      d="M10 13.75C9.58579 13.75 9.25 13.4142 9.25 13C9.25 12.5858 9.58579 12.25 10 12.25C10.4142 12.25 10.75 12.5858 10.75 13C10.75 13.4142 10.4142 13.75 10 13.75Z"
+      fill="currentColor"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M11.5 14.5H8.5C7.94772 14.5 7.5 14.9477 7.5 15.5V18C7.5 18.5523 7.94772 19 8.5 19H11.5C12.0523 19 12.5 18.5523 12.5 18V15.5C12.5 14.9477 12.0523 14.5 11.5 14.5ZM9 17.5V16H11V17.5H9Z"
+      fill="currentColor"
+    />
+    <path
+      d="M5.5 19.5C4.94772 19.5 4.5 19.0523 4.5 18.5C4.5 17.9477 4.94772 17.5 5.5 17.5H14.5C15.0523 17.5 15.5 17.9477 15.5 18.5C15.5 19.0523 15.0523 19.5 14.5 19.5H5.5Z"
+      fill="currentColor"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M19 12H1V11H19V12Z"
+      fill="currentColor"
+    />
+  </svg>
+);
